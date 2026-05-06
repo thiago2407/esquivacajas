5d748a71adf74155314f872b8bc6ce59ac57ed45
thiago2407 <titibin24@gmail.com>
2026-05-06 13:50:50 -0300

    Initial commit

---
+++ Assets/scripts/PlayerControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-2, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(2, 0, 0);
+        }
+    }
+}
